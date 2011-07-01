4b1a68676ddacd714a48e76c380c61ba8f3dc8b0
Meaglin <Meaglin.wasabi@gmail.com>
2011-07-01 04:40:45 +0200

    Reformatting according to a eclipse formatting template(included as formatting.xml).

--- src/RenderEntity.java
+++ src/RenderEntity.java
@@ -6,39 +6,41 @@ import net.minecraft.client.Minecraft;
 
 /**
  * @author lahwran
- *
+ * 
  */
 public class RenderEntity extends sn {
 
-    
     Minecraft mc;
+
     /**
      * @param arg0
      */
-    public RenderEntity(Minecraft mc, fd arg0)
-    {
+    public RenderEntity(Minecraft mc, fd arg0) {
         super(arg0);
-        bM=true; //ignoreFrustumCheck \o/
-        //Do not run debug messages on a live compile!
-        //System.out.println("Entity spawned");
+        bM = true; // ignoreFrustumCheck \o/
+        // Do not run debug messages on a live compile!
+        // System.out.println("Entity spawned");
         this.mc = mc;
     }
 
     @Override
-    protected void a(nu arg0){} //Entity.readEntityFromNBT
+    protected void a(nu arg0) {
+    } // Entity.readEntityFromNBT
 
     @Override
-    protected void b(){} //Entity.entityInit
+    protected void b() {
+    } // Entity.entityInit
 
     @Override
-    protected void b(nu arg0){}
-    
+    protected void b(nu arg0) {
+    }
+
     @Override
-    public void w_()
-    {
+    public void w_() {
         this.d(mc.h.aM, mc.h.aN, mc.h.aO);
     }
-    
+
     @Override
-    public void K() {}
+    public void K() {
+    }
 }
